1a0bf9a1ee300565b7a2cc8f2c06c54af7bd7b4b
Carlos Daniel <118182410+MrRastro@users.noreply.github.com>
2024-09-18 20:24:00 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,56 +1,48 @@
-// Classe Funcionario
-public class Carro{
+public class Carro {
     private String marca;
     private String modelo;
     private int ano;
     private double preco;
-    
-    public Carro(String marca, String modelo, int ano, double preco){
+
+    public Carro(String marca, String modelo, int ano, double preco) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
-        this.preco = preco;
+        setPreco(preco); 
     }
 
-    public String getMarca(){
+    public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca){
-        this.marca = marca;        
-    }
-
-    public String getModelo(){
+    public String getModelo() {
         return modelo;
     }
 
-    public void setModelo(String modelo){
-        this.modelo = modelo;        
-    }
-
-    public int getano(int ano){
+    public int getAno() {
         return ano;
     }
 
-    public void setano(int ano){
-        this.ano = ano;        
-    }
-
-
-    public double getPreco(double preco){
+    public double getPreco() {
         return preco;
     }
 
-    public void setPreco(double preco){
-        if (preco > 0){
-            this.preco = preco;
-        }
-        
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
-    public void exibir(){
-        System.out.println("Lançamento da Marca: " + marca);
-        System.out.println("Lançamento do Modelo: " + modelo);
-        System.out.println("Ano de Lançamento: " + ano);
-        System.out.println("Preço de Lançamento: " + preco);
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setPreco(double preco) {
+        if (preco < 0) {
+            throw new IllegalArgumentException("O preço não pode ser negativo.");
+        }
+        this.preco = preco;
     }
 }
