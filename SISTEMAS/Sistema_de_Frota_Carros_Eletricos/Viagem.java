4c4f888f9fdf585b52926d298d02dbd3ed9d53cc
Carlos Daniel <118182410+CarlosDanielMS@users.noreply.github.com>
2024-11-06 15:45:52 -0300

    Update Viagem.java

--- SISTEMAS/Sistema_de_Frota_Carros_Eletricos/Viagem.java
+++ SISTEMAS/Sistema_de_Frota_Carros_Eletricos/Viagem.java
@@ -8,7 +8,7 @@ public class Viagem {
     private String destino;
     private ArrayList<Eletroposto> paradasRecarga;
 
-    public Viagem(Motorista motorista, CarroEletrico carro, double quilometragem, String destino) {
+    public Viagem(Motorista motorista, CarroEletrico carro, double quilometragem, String destino){
         this.motorista = motorista;
         this.carro = carro;
         this.quilometragem = quilometragem;
@@ -16,8 +16,8 @@ public class Viagem {
         this.paradasRecarga = new ArrayList<>();
     }
 
-    public void adicionarParadaRecarga(Eletroposto eletroposto) {
-        if (eletroposto.getVagasDisponiveis() > 0) {
+    public void adicionarParadaRecarga(Eletroposto eletroposto){
+        if (eletroposto.getVagasDisponiveis() > 0){
             paradasRecarga.add(eletroposto);
             eletroposto.reduzirVaga();
             System.out.println("Parada " + eletroposto.getLocalizacao() + " adicionada com sucesso.");
@@ -26,9 +26,9 @@ public class Viagem {
         }
     }
 
-    public void realizarViagem() {
+    public void realizarViagem(){
         double autonomiaAtual = carro.getAutonomiaAtual();
-        if (autonomiaAtual < quilometragem) {
+        if (autonomiaAtual < quilometragem){
             System.out.println("Autonomia insuficiente. Paradas para recarga são necessárias.");
             // Lógica para verificar eletropostos ao longo do caminho
         } else {
@@ -36,11 +36,11 @@ public class Viagem {
         }
     }
 
-    public Motorista getMotorista() {
+    public Motorista getMotorista(){
         return motorista;
     }
     
-    public void exibirViagem() {
+    public void exibirViagem(){
         System.out.println("Viagem {" +
             " Motorista: " + motorista +
             ", Carro: " + carro +
