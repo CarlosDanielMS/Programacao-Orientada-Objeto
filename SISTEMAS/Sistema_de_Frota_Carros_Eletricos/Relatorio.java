88acc49f1480b631a001c1f5e5b0b5366f7cf97a
Carlos Daniel <118182410+CarlosDanielMS@users.noreply.github.com>
2024-11-06 16:12:57 -0300

    Update Relatorio.java

--- SISTEMAS/Sistema_de_Frota_Carros_Eletricos/Relatorio.java
+++ SISTEMAS/Sistema_de_Frota_Carros_Eletricos/Relatorio.java
@@ -1,29 +1,29 @@
 import java.util.ArrayList;
 
-public class Relatorio {
-    public static void listarVeiculosBaixaAutonomia(ArrayList<CarroEletrico> frota) {
+public class Relatorio{
+    public static void listarVeiculosBaixaAutonomia(ArrayList<CarroEletrico> frota){
         System.out.println("Veículos com autonomia inferior a 20%:");
-        for (CarroEletrico carro : frota) {
-            if (carro.getAutonomiaAtual() < carro.getCapacidadeBateria() * 0.2) {
+        for (CarroEletrico carro : frota){
+            if (carro.getAutonomiaAtual() < carro.getCapacidadeBateria() * 0.2){
                 System.out.println(carro);
             }
         }
     }
 
-    public static void listarViagensMotorista(ArrayList<Viagem> viagens, Motorista motorista) {
+    public static void listarViagensMotorista(ArrayList<Viagem> viagens, Motorista motorista){
         System.out.println("Viagens realizadas por " + motorista.getNome() + ":");
-        for (Viagem viagem : viagens) {
-            if (viagem.getMotorista().equals(motorista)) {
+        for (Viagem viagem : viagens){
+            if (viagem.getMotorista().equals(motorista)){
                 System.out.println(viagem);
             }
         }
     }
     
     // Método para listar histórico de recargas de um carro específico
-    public static void listarHistoricoRecargas(ArrayList<Recarga> recargas, CarroEletrico carro) {
+    public static void listarHistoricoRecargas(ArrayList<Recarga> recargas, CarroEletrico carro){
         System.out.println("Histórico de recargas: " + carro.getModelo());
-        for (Recarga recarga : recargas) {
-            if (recarga.getCarro().equals(carro)) {
+        for (Recarga recarga : recargas){
+            if (recarga.getCarro().equals(carro)){
                 System.out.println(recarga);
             }
         }
