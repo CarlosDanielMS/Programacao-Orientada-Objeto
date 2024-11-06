6d591fe4c5a3ba7bcddf035b2c8b1b5e44bb2947
Carlos Daniel <118182410+CarlosDanielMS@users.noreply.github.com>
2024-11-06 14:16:59 -0300

    Update CarroCompacto.java

--- SISTEMAS/Sistema_de_Frota_Carros_Eletricos/CarroCompacto.java
+++ SISTEMAS/Sistema_de_Frota_Carros_Eletricos/CarroCompacto.java
@@ -2,8 +2,7 @@ public class CarroCompacto extends CarroEletrico {
     public CarroCompacto(String id, String marca, String modelo, int anoFabricacao, double capacidadeBateria, double autonomiaMaxima) {
         super(id, marca, modelo, anoFabricacao, capacidadeBateria, autonomiaMaxima);
     }
-
-    @Override
+    
     public int getTempoMedioCarga() {
         return 4; // Tempo de carga para Carro Compacto é 4 horas
     }
